1d5ce004223b0169b74e496eebda346dbbdd7091
philipontheroof <122261267+philipontheroof@users.noreply.github.com>
2023-06-19 22:02:03 +0900

    final

--- practice.py
+++ practice.py
@@ -1,7 +1,8 @@
-from random import random
+from random import random, seed as random_seed
 import math
 from trafficSimulator import *
 from trafficSimulator.core.geometry.circular_curve import CircularCurve
+import numpy as np
 
 sim = Simulation()
 
@@ -32,29 +33,19 @@ vehicles = []
 km = 1000
 h = 3600
 
-# sim.create_circular_curve((0, 0), 100.0, 0, 2 * math.pi)
-
-# for t in range(22):
-#     vehicle = (0.1 * t, {'path': path, 'v': urb(5.0, 15.0), 'v_max': 120 * km / h,
-#                'a_max': urb(0.3, 2.0), 'b_max': urb(1.5, 2.5), 'T': urb(0.8, 2.0),
-#                          'T_res': 1.0, 's0': urb(2.0, 5.0)})
-#     vehicles.append(vehicle)
-
-# # vehicles.append((10.0, {'path': path, 'v': v, 'v_max': 50.0,
-# #                         'a_max': 25.0, 'b_max': 10.0, 'T': 5.0}))
-
-# vg = FiniteVehicleGenerator({
-#     'vehicles': vehicles,
-# })
-# sim.add_vehicle_generator(vg)
-
-
 cur = CircularCurve((0, 0), 50.0, 0, 2 * math.pi)
 
-for i in range(21, -1, -1):
-    vehicles.append(Vehicle(config={'id': i, 'path': path, 'v': urb(5.0, 15.0), 'v_max': 120 * km / h,
-                                    'a_max': urb(0.3, 2.0), 'b_max': urb(1.5, 2.5), 'T': urb(0.8, 2.0),
-                                    'T_res': 0.0, 's0': urb(2.0, 5.0), 'x': cur.get_length() * i / 22.0}))
+random_seed(0)
+
+N = 22
+for i in range(N - 1, -1, -1):
+    x = cur.get_length() * i / N + np.random.uniform(-3.0, 3.0)
+    if x < 0:
+        x = 0
+    vehicles.append(Vehicle(
+        config={'id': i, 'path': path, 'v': urb(5.0, 15.0), 'v_max': 120 * km / h,
+                'a_max': 4.0, 'b_max': 8.0, 'T': 1.3,
+                'T_res': 0.5, 's0': 3.0, 'x': cur.get_length() * i / N}))
 
 sim.add_segment(cur)
 
--- trafficSimulator/core/vehicle.py
+++ trafficSimulator/core/vehicle.py
@@ -12,15 +12,20 @@ class Vehicle:
         for attr, val in config.items():
             setattr(self, attr, val)
 
+        if self.T_res > 0:
+            self.records = deque([], maxlen=int(self.T_res / self.dt))
+        else:
+            self.records = deque([], maxlen=1)
+
         # Calculate properties
         self.init_properties()
 
     def set_default_config(self):
         self.id = uuid.uuid4()
+        self.method = 'idm'
         self.dt = 1 / 60
 
         self.T_res = 0.7
-        self.records = deque([], maxlen=int(self.T_res / self.dt))
 
         self.l = 3.6
         self.s0 = 10.0
@@ -37,11 +42,17 @@ class Vehicle:
         self.a = 0
         self.stopped = False
 
+        self.T_rel = 1.5
+
     def init_properties(self):
         self.sqrt_ab = 2 * np.sqrt(self.a_max * self.b_max)
         self._v_max = self.v_max
 
+    def ovm(self, delta_x, self_v):
+        return 10 * ((np.tanh((delta_x - 2)) + np.tanh(2)))
+
     def update(self, lead, dt):
+
         # Update position and velocity
         if self.v + self.a * dt < 0:
             self.x -= 1 / 2 * self.v * self.v / self.a
@@ -50,23 +61,33 @@ class Vehicle:
             self.v += self.a * dt
             self.x += self.v * dt + self.a * dt * dt / 2
 
-        # Update acceleration
-        alpha = 0
-        if lead:
+        if self.method == 'idm':
+            # Update acceleration
+            alpha = 0
+            if lead:
+                delta_x = lead.x - self.x - lead.l
+                delta_v = self.v - lead.v
+
+                alpha = (self.s0 + max(0, self.T * self.v +
+                                       delta_v * self.v / self.sqrt_ab)) / delta_x
+
+            a = self.a_max * (1 - (self.v / self.v_max)**4 - alpha**2)
+
+            if self.stopped:
+                self.a = -self.b_max * self.v / self.v_max
+        elif self.method == 'gazis':
             delta_x = lead.x - self.x - lead.l
             delta_v = self.v - lead.v
+            a = delta_v * self.v / delta_x / self.T_rel
+        elif self.method == 'ovm':
+            delta_x = lead.x - self.x - lead.l
+            a = self.ovm(delta_x, self.v)
 
-            alpha = (self.s0 + max(0, self.T * self.v +
-                     delta_v * self.v / self.sqrt_ab)) / delta_x
-
-        a = self.a_max * (1 - (self.v / self.v_max)**4 - alpha**2)
         self.records.append(a)
         self.a = self.records[0]
 
-        if self.stopped:
-            self.a = -self.b_max * self.v / self.v_max
-
     def update_outside(self, lead, dt, lead_x):
+
         # Update position and velocity
         if self.v + self.a * dt < 0:
             self.x -= 1 / 2 * self.v * self.v / self.a
@@ -74,22 +95,32 @@ class Vehicle:
         else:
             self.v += self.a * dt
             self.x += self.v * dt + self.a * dt * dt / 2
+        if self.method == 'idm':
+            # Update acceleration
+            alpha = 0
+            if lead:
+                delta_x = lead_x - lead.l
+                delta_v = self.v - lead.v
 
-        # Update acceleration
-        alpha = 0
-        if lead:
+                alpha = (self.s0 + max(0, self.T * self.v +
+                                       delta_v * self.v / self.sqrt_ab)) / delta_x
+
+            a = self.a_max * (1 - (self.v / self.v_max)**4 - alpha**2)
+            self.records.append(a)
+            self.a = self.records[0]
+
+            if self.stopped:
+                self.a = -self.b_max * self.v / self.v_max
+
+        elif self.method == 'gazis':
             delta_x = lead_x - lead.l
             delta_v = self.v - lead.v
-
-            alpha = (self.s0 + max(0, self.T * self.v +
-                     delta_v * self.v / self.sqrt_ab)) / delta_x
-
-        a = self.a_max * (1 - (self.v / self.v_max)**4 - alpha**2)
+            a = delta_v * self.v / delta_x / self.T_rel
+        elif self.method == 'ovm':
+            delta_x = lead_x - lead.l
+            a = self.ovm(delta_x, self.v)
         self.records.append(a)
         self.a = self.records[0]
-
-        if self.stopped:
-            self.a = -self.b_max * self.v / self.v_max
 
     def stop(self):
         print(f'Vehicle {str(self.id)[-2:]} stopping')
